cf3adea239955c10e90ecfa3e6e601a3482166e9
Joël Elunda <joel.elunda.01@gmail.com>
2020-06-27 01:25:56 +0200

    Home Views UI v1.0

--- application/controllers/Dealer.php
+++ application/controllers/Dealer.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dealer extends CI_Controller {
  
 	public function index() {
-		$this->load->view('dealer/register');
+		// $this->load->view('dealer/register');
+		$this->load->view('index');
 	}
  
 	public function product() {
